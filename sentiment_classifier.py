309572ca56805534056948c88c6de5c4a07c6a19
Dawid <dawid@pluscode.io>
2021-02-25 22:26:37 +0100

    some minor changes

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -7,3 +7,22 @@ for gpu in gpus:
 
 dir_path = "/home/dawidkubicki/Datasets/IMDB"
 
+import pandas as pd
+
+train_data = pd.read_csv("/home/dawidkubicki/Datasets/IMDB/Train.csv")
+test_data = pd.read_csv("/home/dawidkubicki/Datasets/IMDB/Test.csv")
+valid_data = pd.read_csv("/home/dawidkubicki/Datasets/IMDB/Valid.csv")
+
+def clean_text(text):
+    text = text.lower()
+    return text
+
+train_data["text"] = train_data["text"].apply(lambda x: clean_text(x)) 
+
+X_train = train_data["text"]
+y_train = train_data["label"]
+
+X_valid = valid_data["text"]
+y_valid = valid_data["label"]
+
+
