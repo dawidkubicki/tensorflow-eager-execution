85211d85b933eefeaee3489b5a3376992c0378ff
Dawid Kubicki <dawid@pluscode.io>
2021-06-08 00:16:07 +0200

    model added

--- MNIST-tf-eager-execution.py
+++ MNIST-tf-eager-execution.py
@@ -17,6 +17,7 @@ x_test = x_test[..., tf.newaxis].astype("float32")
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(32)
 test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
+#unfold with space :)
 #define the model
 
 class CNNModel(tf.keras.Model):
@@ -35,3 +36,62 @@ class CNNModel(tf.keras.Model):
 
 #create an instance of a class
 model = CNNModel()
+
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = tf.keras.optimizers.Adam()
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
+
+
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        preds = model(images, training=True)
+        loss = loss_object(labels, preds)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    train_loss(loss)
+    train_accuracy(labels, preds)
+
+
+@tf.function
+def test_step(images, labels):
+    # training=False is only needed if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    preds = model(images, training=False)
+    t_loss = loss_object(labels, preds)
+    test_loss(t_loss)
+    test_accuracy(labels, preds)
+
+EPOCHS = 5
+
+import time
+
+start_time = time.time()
+
+for epoch in range(EPOCHS):
+
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    for images, labels in train_ds:
+        train_step(images, labels)
+
+    for test_images, test_labels in test_ds:
+        test_step(test_images, test_labels)
+
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result()}, '
+        f'Accuracy: {train_accuracy.result() * 100}, '
+        f'Test Loss: {test_loss.result()}, '
+        f'Test Accuracy: {test_accuracy.result() * 100}'
+    )
+print(f'Traning took {time.time() - start_time / 60} mins')
